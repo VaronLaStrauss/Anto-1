8a8345539f8191db98aa86d7c7ad800aeee6191b
Viany Manuel <jeangmanuel@gmail.com>
2022-10-19 23:57:22 +0200

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.lblCountry = New System.Windows.Forms.Label()
+        Me.txtCapital = New System.Windows.Forms.TextBox()
+        Me.lblAnswer = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(340, 366)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubmit.TabIndex = 0
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'lblCountry
+        '
+        Me.lblCountry.AutoSize = True
+        Me.lblCountry.Location = New System.Drawing.Point(324, 179)
+        Me.lblCountry.Name = "lblCountry"
+        Me.lblCountry.Size = New System.Drawing.Size(0, 15)
+        Me.lblCountry.TabIndex = 1
+        '
+        'txtCapital
+        '
+        Me.txtCapital.Location = New System.Drawing.Point(324, 237)
+        Me.txtCapital.Name = "txtCapital"
+        Me.txtCapital.Size = New System.Drawing.Size(100, 23)
+        Me.txtCapital.TabIndex = 2
+        '
+        'lblAnswer
+        '
+        Me.lblAnswer.AutoSize = True
+        Me.lblAnswer.Location = New System.Drawing.Point(417, 178)
+        Me.lblAnswer.Name = "lblAnswer"
+        Me.lblAnswer.Size = New System.Drawing.Size(0, 15)
+        Me.lblAnswer.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblAnswer)
+        Me.Controls.Add(Me.txtCapital)
+        Me.Controls.Add(Me.lblCountry)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents lblCountry As Label
+    Friend WithEvents txtCapital As TextBox
+    Friend WithEvents lblAnswer As Label
+End Class
---
+++ WinFormsApp1/Form1.vb
@@ -0,0 +1,81 @@
+﻿Imports System.Threading
+
+
+Public Class Form1
+    ' Use a dictionary instead of a list
+    ' dictionaries are key-value pairs which help in identifying unique sets of data
+    ' keys are used to get the value
+    ReadOnly countries As New Dictionary(Of String, String) From
+    {
+        {"Philippines", "Manila"},
+        {"Austria", "Vienna"},
+        {"Switzerland", "Bern"}
+    }
+    ' Timeout is used as a callback which is called after running the pause
+    ' The callback is a method/function which handles what logic should be used
+    ' after the timeout succeeds
+    ' in this case, we use the intializeCountryInvoke as the logic that handles
+    ' what happens after the timeout (e.g., after 3 seconds)
+    ReadOnly onTimeout As TimerCallback = AddressOf initializeCountryInvoke
+    ' generator is a random integer generator
+    ReadOnly generator As System.Random = New System.Random()
+
+    ' currentCountry holds the current value for checking the current state
+    Dim currentCountry As KeyValuePair(Of String, String) = Nothing
+
+    ' this is a constructor which initializes or "bootstraps" or "starts" (if you will) the class
+    Public Sub New()
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        pickCountry()
+        lblCountry.Text = currentCountry.Key
+    End Sub
+
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+
+        Dim message As String
+
+        ' Checks if currentCountry's capital (the value) is equal to the one written
+        If txtCapital.Text.ToLower() = currentCountry.Value.ToLower() Then
+            message = "Correct!"
+        Else
+            message = "Wrong " + currentCountry.Value
+        End If
+
+        lblAnswer.Text = message
+
+        ' the timer is run, and after 3seconds would run onTimeout property above
+        Dim t As New Timer(onTimeout, Nothing, 3000, -1)
+
+    End Sub
+
+    ' this just picks a random country
+    Private Sub pickCountry()
+        ' gets the total number of elements
+        Dim count As UInt16 = countries.Count
+
+        ' gets a random key-value pair
+        Dim kv = countries.ElementAt(generator.Next(0, count))
+        ' and then deletes it from the list
+        countries.Remove(kv.Key)
+
+        ' sets the key-value pair to the currentCountry so that it's visible
+        ' to the whole class, and we can use for identifying correct answer later
+        currentCountry = kv
+    End Sub
+
+
+    Private Sub initializeCountryInvoke()
+        ' pick another country after running the timeout
+        pickCountry()
+
+        ' Joins threads, error pops out because callback functions are handled by a different thread
+        lblCountry.Invoke(New MethodInvoker(Sub() lblCountry.Text = currentCountry.Key))
+        lblAnswer.Invoke(New MethodInvoker(Sub() lblAnswer.Text = ""))
+        txtCapital.Invoke(New MethodInvoker(Sub() txtCapital.Clear()))
+    End Sub
+
+End Class
